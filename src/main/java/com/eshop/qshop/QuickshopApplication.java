6a9b07ec5fae5748b04b92b73ddcaa167b7be3ee
bhumukulral <bhumukulraj.official@gmail.com>
2022-10-12 21:41:48 +0530

    first commit

---
+++ src/main/java/com/eshop/qshop/QuickshopApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.qshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshop/qshop/QuickshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.qshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
